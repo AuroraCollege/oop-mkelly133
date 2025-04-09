5292390775a6c8adb44a3d52563b3ef034a09e18
Michael Kelly <92009909+mkelly133@users.noreply.github.com>
2025-04-09 23:30:28 +0000

    Added polymorphism

--- vehicles.py
+++ vehicles.py
@@ -1,13 +1,25 @@
 class Vehicle:
-    def __init__(self, fuel):
+    def __init__(self, fuel, mileage):
         self.fuel = fuel
+        self.mileage = mileage
+
+    def return_info(self):
+        return f'Fuel: {self.fuel}, Mileage: {self.mileage}'
 
 class Motorbike(Vehicle):
-    pass
+    def __init__(self, fuel, mileage, hours):
+        self.hours = hours
+        super().__init__(fuel, mileage)
+
+    def return_info(self):
+        return super().return_info() + f', Hours: {self.hours}'
 
 class Car(Vehicle):
     pass
 
-ninja = Motorbike('petrol')
-print(ninja.fuel)
+ninja = Motorbike('petrol', 5000, 25)
+carolla = Car('petrol',780000)
+garage = [ninja, carolla]
+for vehicle in garage:
+    print(vehicle.return_info())
 
